f809ea40aafa246f344704aa439dabe0e7bf55ca
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-09 23:58:22 +0530

    changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,30 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        
+        Product p = new Product();
+        int ans = p.product(3,4);
+        int ans1 = p.product(4,5,6);
+        double ans2 = p.product(4.0,5.0);
+
+    }
+
+
+    //mmm
+    public static class Product{
+
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
 }
